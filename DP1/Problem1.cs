e547a6e7f74d8785aa6be8642f1570816359cc4f
Mohammed-Osama <mallourd16@yahoo.com>
2023-11-29 15:22:24 +0200

    0

---
+++ DP1/Problem1.cs
@@ -0,0 +1,89 @@
+﻿namespace zoik
+{
+    class Problem1
+    {
+        public static void Main(string[] args)
+        {
+            Duck duck = new FightedDucke();
+            duck.perFormFly();
+            duck.perFormQuacke();
+        }
+    }
+}
+
+/**************************************************/
+
+public interface IFlyable {
+    void Fly();
+}
+public interface IQuakeable {
+    void Quake();
+
+}
+
+public class FlyRocket : IFlyable
+{
+    public void Fly()
+    {
+        Console.WriteLine("FlyRocket Bw BW");
+    }
+}
+public class FlyWings : IFlyable
+{
+    public void Fly()
+    {
+        Console.WriteLine("FlyWings wnn");
+    }
+}
+public class NotFling : IFlyable
+{
+    public void Fly()
+    {
+        Console.WriteLine("fuck cant fly");
+    }
+}
+
+public class RabberQuake : IQuakeable
+{
+    public void Quake()
+    {
+        Console.WriteLine(" RabberQuake ");
+    }
+}
+public class Saykak : IQuakeable
+{
+    public void Quake()
+    {
+        Console.WriteLine(" Saykak ");
+
+    }
+}
+public class NotQuake : IQuakeable
+{
+    public void Quake()
+    {
+        Console.WriteLine("Fuck cant Quake");
+    }
+}
+public abstract class Duck
+{
+    public IQuakeable quake { get; set; }
+    public IFlyable flyable { get; set; }
+
+    public void perFormQuacke () {
+        quake.Quake();
+    }
+    public void perFormFly() { 
+        flyable.Fly();
+    }
+
+   
+}
+public class FightedDucke : Duck
+{
+    public  FightedDucke()
+    {
+        quake = new Saykak ();
+        flyable = new FlyRocket ();
+    }
+}
