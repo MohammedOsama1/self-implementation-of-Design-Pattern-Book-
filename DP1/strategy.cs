02d434037c0619fd8345361c8686d36f8c495224
Mohammed-Osama <mallourd16@yahoo.com>
2023-12-04 17:06:02 +0200

    implementation of obervable pattern

---
+++ DP1/strategy.cs
@@ -0,0 +1,66 @@
+﻿namespace zoik
+{
+    class Problem1
+    {
+        public static void Main(string[] args)
+        {
+            Duck duck = new fighDuck();
+            duck.doQuake();
+            duck.doFly();
+        }
+    }
+}
+
+/**************************************************/
+
+
+public interface IFlyable
+{
+    void PerformFly();
+}
+
+public interface IQuakeable
+{
+    void PerformQuake();
+}
+
+public class ICanFly : IFlyable
+{
+    public void PerformFly()
+    {
+        Console.WriteLine("Ican fly");
+    }
+}
+
+public class ICanQuake : IQuakeable
+{
+    public void PerformQuake()
+    {
+        Console.WriteLine("Ican Quake");
+    }
+
+
+}
+
+public abstract class Duck
+{
+    public IQuakeable quake;
+    public IFlyable fly;
+
+    public void doQuake ()
+    {
+        quake.PerformQuake();
+    }
+    public void doFly () {
+        fly.PerformFly();
+    }
+}
+
+public class fighDuck : Duck
+{
+    public fighDuck()
+    {
+        quake = new ICanQuake();
+        fly = new ICanFly();
+    }
+}
